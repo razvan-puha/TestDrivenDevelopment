efda87bb67dd460de413d50b06fc572fb6d2b9c5
razvan-puha <puha.razvan@gmail.com>
2017-03-20 23:00:13 +0200

    Initial commit

---
+++ src/main/MathService.java
@@ -0,0 +1,9 @@
+package main;
+
+public class MathService 
+{
+    public static boolean isLeapYear(int year) 
+    {
+        return year % 4 == 0;
+    }
+}
---
+++ src/main/StringService.java
@@ -0,0 +1,13 @@
+package main;
+
+public class StringService 
+{
+    public boolean isPalindrome(String input) 
+    {
+        StringBuilder sb = new StringBuilder(input);
+
+        String reversedInput = sb.reverse().toString();
+
+        return reversedInput.equals(input);
+    }
+}
