ac17b491defcd8165baa9839243c39faf870fa9c
razvan-puha <puha.razvan@gmail.com>
2017-03-20 23:38:13 +0200

    Write test for divisibility by 100

--- src/main/MathServiceTest.java
+++ src/main/MathServiceTest.java
@@ -1,6 +1,8 @@
 package main;
 
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -10,6 +12,17 @@ public class MathServiceTest
     // a year is a leap year if is divisible by 4
     // but, years divisible by 100 are not leap years
     // except years divisible by 400
+	@BeforeEach
+	public void setUp()
+	{
+		// Nothing to do 
+	}
+	
+	@AfterEach
+	public void tearDown()
+	{
+		// Nothing to do
+	}
 
     @Test
     public void leap_year_should_be_divisible_by_four(){
@@ -19,5 +32,11 @@ public class MathServiceTest
     @Test
     public void non_leap_year_should_not_be_divisible_by_four(){
         assertFalse(MathService.isLeapYear(3));
+    }
+    
+    @Test
+    public void leapYearShouldNotBeDivisibleByOneHundredTest()
+    {
+    	assertFalse(MathService.isLeapYear(2100));
     }
 }
