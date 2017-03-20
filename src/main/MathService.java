67c38571b590c0b76818bbd65c383bc32608082a
razvan-puha <puha.razvan@gmail.com>
2017-03-20 23:49:40 +0200

    Refactor isLeapYear method

--- src/main/MathService.java
+++ src/main/MathService.java
@@ -4,6 +4,9 @@ public class MathService
 {
     public static boolean isLeapYear(int year) 
     {
+    	if(year < 0)
+    		year = year * (-1) + 1;
+    	
     	if(year % 400 == 0)
     		return true;
     	
